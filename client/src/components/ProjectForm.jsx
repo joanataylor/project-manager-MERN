631b9bad3dcd8467082168793ad0bf59c08b494f
Joana Grave <joana_grave@hotmail.com>
2023-01-23 19:03:52 -0500

    initial commit

--- client/src/components/ProjectForm.jsx
+++ client/src/components/ProjectForm.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 
 function ProjectForm() {
   const [item, setItem] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
 
   return (
     <div className="card">
@@ -21,8 +23,36 @@ function ProjectForm() {
               onChange={(e) => setItem(e.target.value)}
             />
           </div>
+          <div className="mb-3">
+            <label htmlFor="price" className="form-label">
+              Price:
+            </label>
+            <input
+              type="number"
+              name="price"
+              id="price"
+              className="form-control"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
+              Description:
+            </label>
+            <input
+              type="text"
+              name="description"
+              id="description"
+              className="form-control"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
           <div className="d-flex justify-content-end">
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
           </div>
         </form>
       </div>
